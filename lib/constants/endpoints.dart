f445ac0f0664cd53dde6998fa360d7ba0f5085e1
Eren GUN <erengun4@gmail.com>
2023-09-10 14:44:40 +0300

    implement endpoints in static class

---
+++ lib/constants/endpoints.dart
@@ -0,0 +1,8 @@
+class Endpoints {
+  /// Endpoints._() prevents instantiation of the class
+  Endpoints._();
+  static String baseUrl = 'https://api.exchangeratesapi.io/v1/';
+  /// For safety, the access key should be stored in a .env file
+  static String accessKey = '766f6d9374dff8b54dc0715b513b385c';
+  /// https://codewithandrea.com/articles/flutter-api-keys-dart-define-env-files/
+}
